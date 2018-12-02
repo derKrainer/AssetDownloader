a6ccda21e1d702361d4e89148b3c41e1fc7a2cc2
Kenny <derkrainer@gmail.com>
2018-12-02 22:19:42 +0100

    use java.time.Duration instead of doing it by myself

--- src/parser/DashParser.java
+++ src/parser/DashParser.java
@@ -1,5 +1,6 @@
 package parser;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -136,105 +137,8 @@ public class DashParser implements IParser
 
   public static double parseDuration(String date)
   {
-    // maybe it's already a number?
-    if (date.charAt(0) != 'P')
-    {
-      try
-      {
-        return Double.parseDouble(date);
-      }
-      catch (Exception ex)
-      {
-        // ignore, just try parsing manually
-      }
-    }
-
-    // parse something like P0Y0M0DT0H3M30.000S
-    double duration = 0;
-    char lastChar = '0';
-    StringBuffer currentPart = new StringBuffer();
-    int multiplier = 0;
-    for (int i = date.length() - 1; i >= 0; i--)
-    {
-      switch (date.charAt(i)) {
-      case 'S':
-      {
-        // seconds
-        multiplier = 1;
-        currentPart = new StringBuffer();
-        lastChar = 'S';
-        break;
-      }
-      case 'M':
-      {
-        // minutes or month
-        duration += parseDuration(currentPart) * multiplier;
-        if (lastChar == 'S' || lastChar == '0')
-        {
-          // minutes
-          multiplier = 60;
-        }
-        else
-        {
-          // month
-          // TODO: real month values if needed at some point?
-          multiplier = 3600 * 24 * 30;
-        }
-        currentPart = new StringBuffer();
-        break;
-      }
-      case 'H':
-      {
-        // hours
-        duration += parseDuration(currentPart) * multiplier;
-        multiplier = 3600;
-        currentPart = new StringBuffer();
-        lastChar = 'H';
-        break;
-      }
-      case 'T':
-      case 'P':
-      {
-        // skip PT
-        duration += parseDuration(currentPart) * multiplier;
-        currentPart = new StringBuffer();
-        lastChar = 'T';
-        break;
-      }
-      case 'D':
-      {
-        // day
-        duration += parseDuration(currentPart) * multiplier;
-        currentPart = new StringBuffer();
-        multiplier = 3600 * 24;
-        lastChar = 'D';
-        break;
-      }
-      case 'Y':
-      {
-        duration += parseDuration(currentPart) * multiplier;
-        currentPart = new StringBuffer();
-        multiplier = 3600 * 365;
-        break;
-      }
-      default:
-      {
-        currentPart.insert(0, date.charAt(i));
-      }
-      }
-    }
-    duration += parseDuration(currentPart) * multiplier;
-
-    return duration;
-  }
-
-  private static double parseDuration(StringBuffer sb)
-  {
-    if (sb == null || sb.length() == 0)
-    {
-      return 0;
-    }
-    return Double.parseDouble(sb.toString());
+    Duration d = Duration.parse(date);
+    return d.toMillis() / 1000.0;
   }
 
 }
