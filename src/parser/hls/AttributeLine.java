ff1bf1448868a400816ede8b59bf132629ec5bb8
bitmovin-kenny <stefan.krainer@bitmovin.com>
2019-08-12 13:01:13 +0200

    use regex to parse attributes as urls with commas broke previous handling

--- src/parser/hls/AttributeLine.java
+++ src/parser/hls/AttributeLine.java
@@ -2,58 +2,75 @@ package parser.hls;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AttributeLine
 {
   public String attributeName;
   public Map<String, String> keyValuePairs = new HashMap<>();
 
+  // "forgiving" attribute list psuedo-grammar:
+  // attributes -> keyvalue (',' keyvalue)*
+  // keyvalue   -> key '=' value
+  // key        -> [^=]*
+  // value      -> '"' [^"]* '"' | [^,]*
+  private static Pattern attributePattern = Pattern.compile("(?:^|,)((?:[^=]*)=(?:\"[^\"]*\"|[^,]*))");
+
   public AttributeLine(String line)
   {
+    String attributeString = this.parseAndRemoveAttributeName(line);
+
+    if (attributeString == null) {
+      return;
+    }
+
+    Matcher attributes = attributePattern.matcher(attributeString);
+
+    boolean found = attributes.find();
+    if (!found) {
+      this.keyValuePairs.put("", attributeString);
+    }
+    else {
+      while(found) {
+        String singleAttributeString = attributeString.substring(attributes.start(), attributes.end());
+        if (singleAttributeString.charAt(0) == ',') {
+          singleAttributeString = singleAttributeString.substring(1);
+        }
+        singleAttributeString = singleAttributeString.trim();
+
+        if (singleAttributeString.indexOf("=") > 0) {
+
+          String key = singleAttributeString.substring(0, singleAttributeString.indexOf("=")).toUpperCase();
+          String value = singleAttributeString.substring(singleAttributeString.indexOf("=") + 1);
+          if (value.charAt(0) == '"') {
+            value = value.substring(1);
+          }
+          if (value.charAt(value.length() - 1) == '"') {
+            value = value.substring(0, value.length() - 1);
+          }
+
+
+          keyValuePairs.put(key, value);
+        } else {
+          keyValuePairs.put(singleAttributeString, null);
+        }
+
+        found = attributes.find();
+      }
+    }
+
+  }
+
+  private String parseAndRemoveAttributeName(String line) {
     if (!line.contains(":"))
     {
       this.attributeName = line;
-      return;
+      return null;
     }
 
     this.attributeName = line.substring(0, line.indexOf(":")).toUpperCase();
-    String attributeStr = line.substring(line.indexOf(":") + 1);
-    String[] attributePairs = attributeStr.split(",");
-
-    if (attributePairs.length == 1 && !attributeStr.contains("="))
-    {
-      // if only one attribute pair exists it's in the form of
-      // #EXT-X-VERSION:3
-      // so just one value, put it in as default ""
-      this.keyValuePairs.put("", attributePairs[0]);
-      return;
-    }
-
-    for (String pair : attributePairs)
-    {
-      String[] keyValue = pair.split("=");
-      if (keyValue.length >= 2)
-      {
-        // if there are = in the value, reduce them again to a single value
-        if (keyValue.length > 2)
-        {
-          for (int i = 2; i < keyValue.length; i++)
-          {
-            keyValue[1] += '=' + keyValue[i];
-          }
-        }
-
-        // ensure upper string for further checks and remoe "" around the value
-        int start = 0, end = keyValue[1].length();
-        if (keyValue[1].charAt(0) == '"')
-        {
-          start++;
-          end--;
-        }
-
-        keyValuePairs.put(keyValue[0].toUpperCase(), keyValue[1].substring(start, end));
-      }
-    }
+    return line.substring(line.indexOf(":") + 1);
   }
 
   public String get(String attributeName)
